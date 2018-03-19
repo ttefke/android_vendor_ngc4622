dca8311e413e3230fa97532f814f5a79240f8814
Tobias Tefke <tobias.tefke@gmail.com>
2018-03-19 18:59:29 +0100

    Add install script for patches

---
+++ patches/install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Store this directory
+CWD=$(pwd)
+
+# Get source root from working directory
+TOP=$CWD/../../../
+
+# Patch build/core in order to include our envsetup.sh
+patchdir="build/core"
+
+for dir in $patchdir ; do
+    cd $TOP
+    cd $patchdir
+    git am $CWD/$patchdir/*.patch
+done
+
+# Apply microg patches
+patchdir="frameworks/base"
+
+for dir in $patchdir ; do
+    cd $TOP
+    cd $patchdir
+    git am $CWD/$patchdir/*.patch
+done
+
+# Update checkstyle for Arch Linux
+if [ -f "/etc/arch-release" ]; then
+    patchdir="prebuilts/checkstyle"
+
+    for dir in $patchdir ; do
+        cd $TOP
+        cd $patchdir
+        git am $CWD/$patchdir/*.patch
+    done
+fi
+
+cd $CWD
