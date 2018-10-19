d964fcee3497346eca203bd086b28074e004e48c
Tobias Tefke <tobias.tefke@tutanota.com>
2018-10-19 20:55:12 +0200

    build/envsetup.sh: add dessert() function
    Dessert is a new function similar to brunch. But it does not simply build android.
    It automatically sets up the ram amount available for Jack and sets up ccache.
    Moreover it starts more build threads/core making the whole build a lot faster.
    
    Change-Id: Id05c3eb6ee1cb0f88047639400aa378503b30552

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -10,3 +10,53 @@ fi
 
 # Build su
 export WITH_SU=true
+
+function setupJack() {
+    # get amount of ram in kb
+    ram=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+    # convert to gb
+    ram=$(expr $ram / 1000000)
+    # add gb sign
+    g="G"
+    ram="$ram$g"
+    # export to jack
+    export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx$ram"
+}
+
+function mkd() {
+    # get number of cores
+    cores=$(nproc)
+    # 4 threads per core
+    threads=$(expr $cores \* 4)
+    m -j$threads "$@"
+}
+
+function setupCcache() {
+    export USE_CCACHE=1
+    echo "Enter location for cache files:"
+    read location
+    export CCACHE_DIR=$location
+    echo "Enter max ccache size in GB:"
+    read cachesize
+    g="G"
+    prebuilts/misc/linux-x86/ccache/ccache -M $cachesize$g
+    export NGC_CCACHE_SETUP=1
+}
+
+function dessert()
+{
+    # setup ccache
+    if [ "$NGC_CCACHE_SETUP" != "1" ]; then
+        setupCcache
+    fi
+    # setup jack
+    setupJack
+    breakfast $*
+    if [ $? -eq 0 ]; then
+        mkd bacon
+    else
+        echo "No such item in dessert menu. Try 'breakfast'"
+        return 1
+    fi
+    return $?
+}
