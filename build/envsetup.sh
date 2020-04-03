5fb93531ef5c1817dba1fb5820512d6729e3cae8
Tobias Tefke <tobias@tefke.name>
2020-04-03 14:39:30 +0200

    Store signed images in separate dist directories
    Change-Id: Iddfb69ed424af193f0707b87ad24ef573ffd2b17

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -14,18 +14,27 @@ function setupJack() {
 function dessert()
 {
     setupJack
-    echo "Removing old zip files"
-    rm signed-target_files.zip signed-ota_update.zip
-    breakfast $1
+    DEVICE=$1
+    breakfast $DEVICE
     if [ $? -eq 0 ]; then
         m -j$2 target-files-package otatools
+        if [ -z ${OUT_DIR_COMMON_BASE+x} ]; then
+            OUTPATH=$ANDROID_BUILD_TOP/out/dist/$DEVICE
+            RECOVERY=$ANDROID_BUILD_TOP/out/target/product/$DEVICE/recovery.img
+        else
+            OUTPATH=$OUT_DIR_COMMON_BASE/dist/$DEVICE
+            RECOVERY=$OUT_DIR_COMMON_BASE/target/product/$DEVICE/recovery.img
+        fi
+        mkdir -p $OUTPATH
+        cp $RECOVERY $OUTPATH/recovery_$DEVICE.img
         ./build/tools/releasetools/sign_target_files_apks -o -d ~/.android-certs \
             $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip \
             signed-target_files.zip
         ./build/tools/releasetools/ota_from_target_files -k ~/.android-certs/releasekey \
             --block --backup=true \
             signed-target_files.zip \
-            signed-ota_update.zip
+            $OUTPATH/lineage_signed_${DEVICE}_$(date +"%d-%b-%Y-%H-%M").zip
+        rm signed-target_files.zip
     else
         echo "No such item in dessert menu. Try 'breakfast'"
         return 1
